f6950c077203ef70915c5e422fcce192e5be447b
Marshall Johnston <marshalljohnston77@gmail.com>
2018-06-28 16:47:27 -0400

    fixed addbutton and appending rating to div

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,55 +5,46 @@
 // 3. do i need a remove button?
 // 4. I need to initalize with a gif
 var topics = ["Obama", "Lebron", "Cash", "AFV", "kangaroo", "sloth", "dolphins", "hamster"];
-var newTopic = "Welcome";
+var counter = 0; 
 
 function createButtons() {
     
     $("#buttons").empty(); 
     
     for (var i = 0; i < topics.length; i++) {
-        
+        //just added the css elements this way to see how they all reacted
         var giphyBtn = $("<button>").html(topics[i]);
         giphyBtn.attr("data-name", topics[i]);
-        giphyBtn.css("text-transform", "uppercase")
-        giphyBtn.css("margin", "5px")
-        giphyBtn.css("background-color", "#202A25")
+        giphyBtn.css("text-transform", "uppercase");
+        giphyBtn.css("margin", "5px");
+        giphyBtn.css("background-color", "#202A25");
         giphyBtn.css("color", "white");
+        giphyBtn.css("text-align", "center");
         giphyBtn.addClass("cbutton");
         $("#buttons").append(giphyBtn);
     };
 };
-
-function addButton() {
     
     $("#addGifBtn").on("click", function () {
         event.preventDefault(); 
-    
+
         var newTopic = $("#giphy-input").val().trim(); 
+
+        if (newTopic != "") {
         topics.push(newTopic);
         console.log("new topic: " + newTopic);
         console.log("topics array: " + topics);
         createButtons(); 
         $("#giphy-input").val(""); 
+        } else { 
+            alert("Please do yourself a favour and actually enter something in the input box...")
+        };
     });
-    
-    
-}
-
-
-    $("#removeBtn").on("click", function () {
-        event.preventDefault(); 
-        //remove from the array as opposed to removing from div
-        $("#buttons").empty(); 
-    });
-
+// };
 
 function getGifs() {
     // this is the key to the solution right here I believe
-
     var newTopic = $(this).attr("data-name");
-
-    console.log(newTopic);
     
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newTopic + "&api_key=AGOnLXwDOWiIu3oC7OMWNFsQCMAElFt4&limit=10";
     
@@ -62,17 +53,15 @@ function getGifs() {
         method: "GET"
         
     }).then(function(response){ 
-        console.log(response);
-        $("#gifs").empty(); 
-    
+        $("#gifs").empty();
             
             for (var i = 0; i < response.data.length; i++)  {
                 
                 var theGif = $("<img>");
                 theGif.addClass("theGif");
-                theGif.attr("src",          response.data[i].images.fixed_width_still.url);
-                theGif.attr("data-still",   response.data[i].images.fixed_width_still.url)
-                theGif.attr("data-animate", response.data[i].images.fixed_width.url);
+                theGif.attr("src",          response.data[i].images.fixed_height_still.url);
+                theGif.attr("data-still",   response.data[i].images.fixed_height_still.url)
+                theGif.attr("data-animate", response.data[i].images.fixed_height.url);
                 theGif.attr("data-state", "still");
 
                 // need to find a way to append to a relative position of the img element. 
@@ -81,8 +70,12 @@ function getGifs() {
                 gifRating.addClass("gifRating");
                 gifRating.css("text-transform", "uppercase")  
 
-                $("#gifs").append(theGif);
-                $("#ratings").append(gifRating);
+                var gifContainer = $("<div>").addClass("gif");
+                 gifContainer.append(theGif);
+                 gifContainer.append(gifRating);
+
+                 $("#gifs").append(gifContainer);
+                
                 // gifRating)
                 
             };
@@ -91,7 +84,8 @@ function getGifs() {
     };
     createButtons(); 
     getGifs(); 
-    addButton();
+    // addButton(); 
+
     $(document).on("click", ".cbutton", getGifs);
     $(document).on("click", ".theGif", function() {
 
@@ -100,7 +94,7 @@ function getGifs() {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
         } else { 
-            
+
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", $(this).attr("still"));
             
